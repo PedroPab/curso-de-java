0dbd2afa139d173d2060e6e54237e20f3a5e37e3
Pedro <pmontesarroyave@gmail.com>
2022-02-25 10:51:57 -0500

    compoes activados

--- descunto.js
+++ descunto.js
@@ -9,14 +9,41 @@ function calcularPrecioDescunto(precio , descuento){
 
 function calcularDescuntoProductos(){
     const inputPrice = parseInt(document.getElementById("inputPricioPoducto").value);
-    const inputDescunto = parseInt(document.getElementById("inputDescuentoProducto").value);
+    const inputCupon = document.getElementById("inputCuponDescuento").value;
     
-    const precioConDescunto = calcularPrecioDescunto(inputPrice, inputDescunto);
+    
+    const isCouponValueValid = function (coupon) {
+        return coupon.name === inputCupon;
+    };
+    
+    const userCoupon = coupons.find(isCouponValueValid);
+    console.log(userCoupon);
+    
+    if (!userCoupon) {
+        alert("El cupón " + inputCupon + "no es válido");
+    }else{
+        const descuento = userCoupon.discount;
+        const precioConDescunto = calcularPrecioDescunto(inputPrice, descuento); 
+        const priceP = document.getElementById("priceParrafo");
+        priceP.innerText = "El precio con descunto son : $" + precioConDescunto;
 
-    const priceP = document.getElementById("priceParrafo");
-    priceP.innerText = "El precio con descunto son : $" + precioConDescunto;
-
+    }
+    
 }
+const coupons = [
+    {
+        name: "JuanDC_es_Batman",
+        discount: 15,
+    },
+    {
+        name: "pero_no_le_digas_a_nadie",
+        discount: 30,
+    },
+    {
+        name: "es_un_secreto",
+        discount: 25,
+    },
+];
 
 // console.log({
 //     precioOriginal,
