bb0bc899934ebc3baf6311a6c1837dd291b51ab8
Pedro <pmontesarroyave@gmail.com>
2022-02-27 11:05:11 -0500

    se me olvido guardar

--- analisis.js
+++ analisis.js
@@ -1,19 +1,22 @@
+// helpers
+
+function esPar(numerito){
+    return (numerito % 2 === 0);
+};
+
 const salariosCol = colombia.map(
     function (persona){
         return persona.salary;
     }
 );
-
+//salarios colombia 
 const salariosColSorted = salariosCol.sort(
     function (salarioA , salarioB){
         return salarioA - salarioB ;
     }
 );
 
-function esPar(numerito){
-    return (numerito % 2 === 0);
-};
-
+//promedio
 function calcularPromedio(lista){
     const sumaLista1 = lista.reduce(
         function (valorAcumulado = 0, nuevoElemento){
@@ -24,6 +27,8 @@ function calcularPromedio(lista){
     return promedioLista;
 }
 
+
+//mediana 
 function medianaSalario(lista){
     const mitad = parseInt(lista.length / 2 );
 
@@ -39,4 +44,6 @@ function medianaSalario(lista){
     }
 }
 
+
+//mediana top 10%
 console.log(medianaSalario(salariosColSorted));
